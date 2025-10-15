6cef6c8f0bbe47c42d75de24f478583c71ebb964
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-10-15 05:28:17 -0400

    Refactor main application routing and enhance navigation
    - Updated the App component to utilize HashRouter for improved routing structure.
    - Added navigation links to Settings and Rankings in the header for better user access.
    - Implemented a redirect from the root path to the Rankings page for streamlined navigation.

--- entrypoints/pages/main.tsx
+++ entrypoints/pages/main.tsx
@@ -1,15 +1,8 @@
 import { renderToBody } from "@/utils/render";
 import { For } from "solid-js";
-import { Route, HashRouter } from "@solidjs/router";
+import { Route, HashRouter, A, useNavigate } from "@solidjs/router";
 import { items, useSettings } from "@/utils/storage";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
-}
 const rankings = useSettings(items.rankings);
 function Rankings() {
   return (
@@ -27,16 +20,29 @@ function Rankings() {
 
 function App() {
   return (
-    <div class="bg-background text-foreground min-h-screen w-screen min-w-[320px] p-4">
-      <header class="flex items-center justify-center gap-4 p-4 text-xl shadow-md">
-        <a href="/rankings">Rankings</a>
-        <a href="/">Home</a>
-      </header>
-      <HashRouter>
-        <Route path="/" component={Home} />
-        <Route path="/rankings" component={Rankings} />
-      </HashRouter>
-    </div>
+    <HashRouter
+      root={(props) => (
+        <div class="bg-background text-foreground min-h-screen w-screen min-w-[320px] p-4">
+          <header class="justify-left flex items-center gap-4 p-4 text-xl *:hover:underline">
+            <A href="/settings">Settings</A>
+            <A href="/rankings">Rankings</A>
+          </header>
+          {props.children}
+        </div>
+      )}
+    >
+      <Route
+        path="/"
+        component={() => {
+          const navigate = useNavigate();
+
+          navigate("/rankings", { replace: true });
+          return <></>;
+        }}
+      />
+      <Route path="/settings" component={() => <div>Settings</div>} />
+      <Route path="/rankings" component={Rankings} />
+    </HashRouter>
   );
 }
 
--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -3,7 +3,10 @@ import { renderToBody } from "@/utils/render";
 function App() {
   return (
     <div class="bg-background text-foreground flex h-full w-full min-w-[320px] flex-row gap-2 p-4">
-      <a href={browser.runtime.getURL("/pages.html")} target="_blank">
+      <a
+        href={browser.runtime.getURL("/pages.html") + "#/rankings"}
+        target="_blank"
+      >
         Rankings
       </a>
     </div>
