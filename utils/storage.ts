d73958cd0c7e8e917410f391f12cd7750001f2bc
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-10-16 01:08:25 -0400

    Refactor rankings storage structure to support versioning and migration from v1 to v2. Updated rankings type to include additional properties for enhanced functionality.

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,13 +1,40 @@
 import { storage } from "#imports";
 import { createSignal, onMount, onCleanup } from "solid-js";
 
-const rankings = storage.defineItem<{ [domain: string]: 2 | 1 | 0 | -1 | -2 }>(
-  "sync:rankings",
+type RankingsV1 = { [domain: string]: 2 | 1 | 0 | -1 | -2 };
+
+export type RankingsV2 = Record<
+  string,
   {
-    fallback: {},
-    version: 1,
+    type: "raise" | "lower" | "block" | "none" | "pin";
+    strength?: "weak" | "normal" | "strong" | undefined;
+  }
+>;
+
+const rankings = storage.defineItem<RankingsV2>("sync:rankings", {
+  fallback: {},
+  version: 2,
+  migrations: {
+    2: (rankings: RankingsV1): RankingsV2 => {
+      const convertRank = (rank: RankingsV1[string]) => {
+        const mapper = {
+          [-2]: "block",
+          [-1]: "lower",
+          [0]: "none",
+          [1]: "raise",
+          [2]: "pin",
+        } as const;
+        return { type: mapper[rank] };
+      };
+      return Object.fromEntries(
+        Object.entries(rankings).map(([domain, rank]) => [
+          domain,
+          convertRank(rank),
+        ]),
+      );
+    },
   },
-);
+});
 
 export const items = { rankings };
 
