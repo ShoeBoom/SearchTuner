b0f79050820dd3e0a17181582a4ce57f288b9867
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-12-16 21:40:14 -0500

    Refactor Bangs component for improved data handling and table structure
    - Simplified data retrieval by directly calling `bangsData()` within the component.
    - Rearranged table columns to display triggers before sites for better readability.
    - Removed unnecessary URL column to streamline the table layout.

--- src/entrypoints/pages/components/Bangs.tsx
+++ src/entrypoints/pages/components/Bangs.tsx
@@ -2,32 +2,28 @@ import { For } from "solid-js";
 import { bangsData } from "@/utils/storage";
 
 const Bangs = () => {
-	const data = bangsData();
-	const bangs = () => data?.data?.bangs ?? [];
+	// const data = );
+	const bangs = () => bangsData()?.data?.bangs ?? [];
 
 	return (
 		<div>
-			<table class="w-full max-w-4xl table-auto">
+			<table class="w-full table-auto">
 				<thead class="border-b">
 					<tr>
-						<th class="pb-2 font-semibold text-sm">Trigger</th>
 						<th class="pb-2 font-semibold text-sm">Site</th>
+						<th class="pb-2 font-semibold text-sm">Trigger</th>
 						<th class="pb-2 font-semibold text-sm">Domain</th>
 						<th class="pb-2 font-semibold text-sm">Category</th>
-						<th class="pb-2 font-semibold text-sm">URL</th>
 					</tr>
 				</thead>
 				<tbody>
 					<For each={bangs()}>
 						{(bang) => (
 							<tr class="hover:bg-foreground/10 [&>td]:py-2">
-								<td class="text-center font-mono text-lg">{bang.t}</td>
 								<td class="text-center">{bang.s}</td>
+								<td class="text-center">{bang.t}</td>
 								<td class="text-center">{bang.d}</td>
 								<td class="text-center">{bang.c ?? "-"}</td>
-								<td class="break-all text-center font-mono text-sm">
-									{bang.u}
-								</td>
 							</tr>
 						)}
 					</For>
